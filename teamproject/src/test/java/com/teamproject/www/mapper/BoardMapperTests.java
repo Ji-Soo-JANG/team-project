4992239fc79b092016baf0e53fd3808bcc994877
Ji-Soo-JANG <wltn2101@gmail.com>
2024-08-08 22:13:41 +0900

    BoardMapperTests.java 수정

--- teamproject/src/test/java/com/teamproject/www/mapper/BoardMapperTests.java
+++ teamproject/src/test/java/com/teamproject/www/mapper/BoardMapperTests.java
@@ -14,8 +14,6 @@ import com.teamproject.www.Jang.domain.BoardVo;
 import com.teamproject.www.Jang.domain.NoticeCriteria;
 import com.teamproject.www.Jang.domain.NoticeDto;
 import com.teamproject.www.Jang.mapper.BoardMapper;
-import com.teamproject.www.Lee.domain.board.BoardListDto;
-import com.teamproject.www.Lee.domain.board.Criteria;
 
 import lombok.extern.log4j.Log4j;
 
@@ -23,8 +21,6 @@ import lombok.extern.log4j.Log4j;
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 @Log4j
 public class BoardMapperTests {
-	@Autowired
-	private com.teamproject.www.Lee.mapper.board.BoardMapper leeMapper;
 	
 	@Autowired
 	private BoardMapper boardMapper;
@@ -34,6 +30,7 @@ public class BoardMapperTests {
 		log.info(boardMapper);
 	}
 
+	
 	@Test
 	public void testInsertnotice() {
 		for(int i = 1; i <= 3; i++) {
@@ -44,8 +41,8 @@ public class BoardMapperTests {
 		boardVo.setNickname("nickname-" + i);
 		boardVo.setBoardtype(0); // type 0 - 공지사항
 		
-//		int count = boardMapper.insertNotice(vo);
-		log.info("------------------------------" + vo.getBno() + "--------------------------------");
+		int count = boardMapper.insertNotice(boardVo);
+		log.info("------------------------------" + boardVo.getBoardNo() + "--------------------------------");
 //		System.out.println("------------------------------" + vo.getBno() + "--------------------------------");
 		
 		try {
@@ -55,6 +52,14 @@ public class BoardMapperTests {
 			e.printStackTrace();
 		}
 		}
+	}
+	
+	@Test
+	public void testGetNotice() {
+		Long boardNo = 1841L;
+		NoticeDto dto = boardMapper.getNotice(boardNo);
+		// content - [unread]되더라도 setContent()는 잘 이루어짐
+		log.warn(dto);
 	}
 	
 	@Test
@@ -73,8 +78,8 @@ public class BoardMapperTests {
 	@Test
 	public void testGetTotal() {
 		NoticeCriteria cri = new NoticeCriteria();
-		cri.setType("T");
-		cri.setKeyword("항3");
+		cri.setType("I");
+		cri.setKeyword("name");
 		int count = boardMapper.getTotal(cri);
 		log.info(count);
 	}
@@ -84,10 +89,11 @@ public class BoardMapperTests {
 		NoticeCriteria cri = new NoticeCriteria();
 		cri.setPageNum(1);
 		cri.setAmount(10);
-//		cri.setKeyword("항3");
-//		cri.setType("T");
+		cri.setKeyword("항3");
+		cri.setType("T");
 		
-//		List<NoticeDto> list = boardMapper.getListWithPaging(cri);
+		List<NoticeDto> list = boardMapper.getListWithPaging(cri);
+		System.out.println(list);
 //		for(noticeDto dto : list) {
 //			System.out.println(dto);
 //		}
@@ -95,8 +101,8 @@ public class BoardMapperTests {
 	
 	@Test
 	public void testViewsUp() {
-		Long bno = 497L;
-		int views = boardMapper.viewsUp(bno);
+		Long boardNo = 1900L;
+		int views = boardMapper.viewsUp(boardNo);
 		if(views > 0 ) {
 			log.info(true);
 		}
@@ -104,32 +110,18 @@ public class BoardMapperTests {
 	
 	@Test
 	public void testUpdate() {
-        Long bno = 2821L;
+        Long boardNo = 1900L;
         String content = "<h1>변경됨</h1>";
-        int result = boardMapper.updateNotice(content, bno);
-        NoticeDto dto = boardMapper.getNotice(bno);
+        int result = boardMapper.updateNotice(content, boardNo);
+        NoticeDto dto = boardMapper.getNotice(boardNo);
         log.info(dto);
 	}
 	
 	@Test
 	public void testDelete() {
-		Long bno = 2821L;
-		int result = boardMapper.deleteNotice(bno);
+		Long boardNo = 1900L;
+		int result = boardMapper.deleteNotice(boardNo);
 		log.info(result);
 	}
-	
-	@Test
-	public void leeInstance() {
-		log.info(leeMapper);
-	}
-	
-	@Test
-	public void leeTestGetBoardList() {
-		Criteria cri = new Criteria();
-		cri.setBoardtype(1);
-		log.info("cri : " + cri);
-		List<BoardListDto> list = leeMapper.getListWithPaging(cri);
-		log.info("dto : " + list);
-	} 
 	
 }
