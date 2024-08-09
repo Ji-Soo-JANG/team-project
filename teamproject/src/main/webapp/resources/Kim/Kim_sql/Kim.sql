aaeabe2fb911a81c1826246987d8a6bbe0e45498
105mm <king7532@knou.ac.kr>
2024-08-09 13:44:31 +0900

    김세영 테스트 sql 수정 및 소분류기능 주석처리

--- teamproject/src/main/webapp/resources/Kim/Kim_sql/Kim.sql
+++ teamproject/src/main/webapp/resources/Kim/Kim_sql/Kim.sql
@@ -5,21 +5,15 @@ values(6, '리뷰');
 insert into tbl_board_type
 values(7, '질문게시판');
 
-BEGIN
-    INSERT INTO TBL_USER(userId, userPw, nickname, email) VALUES('rabbit', '1234', '토끼', 'rabbit@email.com');
-    INSERT INTO TBL_USER(userId, userPw, nickname, email) VALUES('lion', '1234', '사자', 'lion@email.com');
-    INSERT INTO TBL_USER(userId, userPw, nickname, email) VALUES('tiger', '1234', '호랑이', 'tiger@email.com');
-    INSERT INTO TBL_USER(userId, userPw, nickname, email) VALUES('bear', '1234', '곰', 'bear@email.com');
-    INSERT INTO TBL_USER(userId, userPw, nickname, email) VALUES('fox', '1234', '여우', 'fox@email.com');
-    INSERT INTO TBL_USER(userId, userPw, nickname, email) VALUES('wolf', '1234', '늑대', 'wolf@email.com');
-    INSERT INTO TBL_USER(userId, userPw, nickname, email) VALUES('cat', '1234', '고양이', 'cat@email.com');
-    INSERT INTO TBL_USER(userId, userPw, nickname, email) VALUES('dog', '1234', '강아지', 'dog@email.com');
-    INSERT INTO TBL_USER(userId, userPw, nickname, email) VALUES('elephant', '1234', '코끼리', 'elephant@email.com');
-    INSERT INTO TBL_USER(userId, userPw, nickname, email) VALUES('giraffe', '1234', '기린', 'giraffe@email.com');
-END;
-/
+-- 기존 시퀀스 삭제
+DROP SEQUENCE seq_board;
 
-COMMIT;
+-- 시퀀스 재생성
+CREATE SEQUENCE seq_board
+    START WITH 1          -- 시작 값
+    INCREMENT BY 1        -- 증가 값
+    NOCACHE               -- 시퀀스 캐싱 안함
+    NOCYCLE;              -- 최대값 도달 시 다시 시작하지 않음
 
 
 
@@ -31,11 +25,13 @@ DECLARE
     v_nicknames nickname_array := nickname_array('토끼', '사자', '호랑이', '곰', '여우', '늑대', '고양이', '강아지', '코끼리', '기린');
     v_userids userid_array := userid_array('rabbit', 'lion', 'tiger', 'bear', 'fox', 'wolf', 'cat', 'dog', 'elephant', 'giraffe');
     v_index PLS_INTEGER;
+    v_boardtypeno NUMBER;
 BEGIN
     FOR i IN 1..505 LOOP
         v_index := TRUNC(DBMS_RANDOM.VALUE(1, 11)); -- 1부터 10까지의 정수 생성
         v_userid := v_userids(v_index);
         v_nickname := v_nicknames(v_index);
+        v_boardtypeno := TRUNC(DBMS_RANDOM.VALUE(5, 8)); -- 5부터 7까지의 정수 생성
 
         INSERT INTO TBL_BOARD (
             BOARDNO, TITLE, CONTENT, USERID, NICKNAME, BOARDTYPENO, REGDATE, UPDATEDATE, VIEWS, LIKES, REPLYCOUNT
@@ -45,7 +41,7 @@ BEGIN
             '글내용-' || i,
             v_userid,
             v_nickname,
-            5,
+            v_boardtypeno,
             SYSDATE,
             SYSDATE,
             0,
@@ -57,4 +53,3 @@ END;
 /
 
 COMMIT;
-
